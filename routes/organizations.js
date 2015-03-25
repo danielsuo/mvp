435e80c2cd5b3f693490210093375108e4712705
Daniel Suo <danielsuo@gmail.com>
2015-03-25 12:58:57 -0400

    add organization deep populate

--- routes/organizations.js
+++ routes/organizations.js
@@ -39,15 +39,43 @@ router.post('/new', isLoggedIn, function(req, res, next) {
   });
 });
 
+var Floor = require('../models/floor');
+var Demising = require('../models/demising');
+var Suite = require('../models/suite');
+var Testfit = require('../models/testfit');
+
 router.get('/:id', isLoggedIn, function(req, res, next) {
   Organization.findById(req.params.id)
+    .lean()
     .populate('buildings')
     .exec(function(err, organization) {
-      if (err) return next(err);
-      res.render('organizations/show.html', {
-        organization: organization
+      Building.populate(organization.buildings, {
+        path: 'floors'
+      }, function(err, buildings) {
+        for (var i = 0; i < buildings.length; i++) {
+          Floor.populate(buildings[i].floors, {
+            path: 'demisings'
+          }, function(err, floors) {
+            for (var j = 0; j < floors.length; j++) {
+              Demising.populate(floors[j].demisings, {
+                path: 'suites'
+              }, function(err, demisings) {
+                for (var k = 0; k < demisings.length; k++) {
+                  Suite.populate(demisings[k].suites, {
+                    path: 'testfits'
+                  }, function(err, suites) {
+                    if (err) return next(err);
+                    res.render('organizations/show.html', {
+                      organization: organization
+                    });
+                  })
+                }
+              })
+            }
+          })
+        }
       })
-    })
+    });
 });
 
 router.get('/:id/buildings/new', isLoggedIn, function(req, res, next) {
